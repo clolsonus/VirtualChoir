4bb41653a7e66581452c8f79f99f463e6f7bb522
Curtis L. Olson <curtolson@flightgear.org>
2020-09-16 13:45:58 -0500

    Some code cleanups and plotting improvements.  Disable some less useful plots/code.

--- vc.py
+++ vc.py
@@ -10,6 +10,7 @@ import numpy as np
 import os
 import pycorrelate as pyc                # pip install pycorrelate
 from pydub import AudioSegment, playback  # pip install pydub
+from scipy import signal
 import sys
 
 parser = argparse.ArgumentParser(description='virtual choir')
@@ -45,51 +46,20 @@ for v in args.videos:
     print(" ", v)
     basename, ext = os.path.splitext(v)
     sample = AudioSegment.from_file(v, ext[1:])
-    sample = sample.set_channels(1) # convert to mono
     sample = sample.normalize()
     samples.append(sample)
-    raw = sample.get_array_of_samples()
+    mono = sample.set_channels(1) # convert to mono
+    raw = mono.get_array_of_samples()
     raws.append(raw)
-
-    if False:
-        hop_length = 512
-        oenv = librosa.onset.onset_strength(y=np.array(raw).astype('float'), sr=sample.frame_rate,
-                                            hop_length=hop_length)
-        tempogram = librosa.feature.tempogram(onset_envelope=oenv,
-                                              sr=sample.frame_rate,
-                                              hop_length=hop_length)
-        print(tempogram.shape, tempogram)
-        plt.figure()
-        times = librosa.times_like(oenv, sr=sample.frame_rate,
-                                   hop_length=hop_length)
-        print(np.mean(oenv), np.std(oenv))
-        plt.plot(times, oenv)
-        plt.show()
-
-    if False:
-        plt.figure()
-        M=1024
-        from scipy import signal
-        dt = 1 / sample.frame_rate
-        freqs, times, Sx = signal.spectrogram(np.array(raw), fs=dt,
-                                              window='hanning',
-                                              nperseg=M, noverlap=M - 100,
-                                              detrend=False, scaling='spectrum')
-        f, ax = plt.subplots()
-        ax.pcolormesh(times, freqs, 10 * np.log10(Sx), cmap='viridis')
-        ax.set_title("Accelerometer Spectogram")
-        ax.set_ylabel('Frequency [Hz]')
-        ax.set_xlabel('Time [s]');
-        plt.show()
 
 # find first clap event in each sample
 print("locating sync clap...")
 clap_offset = []
 min_sync = None
-for i, sample in enumerate(samples):
+for i, raw in enumerate(raws):
     v = args.videos[i]
     raw = raws[i]
-    rate = sample.frame_rate
+    rate = samples[i].frame_rate
     sync_ms = int(round(find_sync_clap(raw, rate)))
     print(" ", args.videos[i], sync_ms)
     clap_offset.append(sync_ms)
@@ -104,35 +74,69 @@ for i in range(len(clap_offset)):
 
 # plot synced signals
 print("plot synced signals...")
-plt.figure(1)
-for i, sample in enumerate(samples):
+fig, ax = plt.subplots(nrows=len(raws), sharex=True, sharey=True)
+for i, raw in enumerate(raws):
     v = args.videos[i]
-    raw = sample.get_array_of_samples()
-    rate = sample.frame_rate
+    rate = samples[i].frame_rate
     sync_ms = clap_offset[i]
+    trimval = int(round(sync_ms * rate / 1000))
     print(" ", args.videos[i], sync_ms)
-    plt.title(basename)
-    hackval = int(round(sync_ms * rate / 1000))
-    plt.plot(np.abs(raw[hackval:]))
+    ax[i].plot(np.array(raw[trimval:]))
 plt.show()
 
+# work with librosa to visualize audio streams
 if True:
-    # work with librosa to sync audio streams
+    # plot beat peaks
+    hop_length = 512
+    fig, ax = plt.subplots(nrows=len(raws), sharex=True, sharey=True)
+    for i, raw in enumerate(raws):
+        sync_ms = clap_offset[i]
+        rate = samples[i].frame_rate
+        trimval = int(round(sync_ms * rate / 1000))
+        oenv = librosa.onset.onset_strength(y=np.array(raw[trimval:]).astype('float'),
+                                            sr=samples[i].frame_rate,
+                                            hop_length=hop_length)
+        tempogram = librosa.feature.tempogram(onset_envelope=oenv,
+                                              sr=samples[i].frame_rate,
+                                              hop_length=hop_length)
+        #print(tempogram.shape, tempogram)
+        times = librosa.times_like(oenv, sr=samples[i].frame_rate,
+                                   hop_length=hop_length)
+        print(np.mean(oenv), np.std(oenv))
+        ax[i].plot(times, oenv)
+
+    if False:
+        # plot raw spectrogram (this doesn't seem as useful as the chroma plot)
+        fig, ax = plt.subplots(nrows=len(raws), sharex=True, sharey=True)
+        for i, raw in enumerate(raws):
+            M=1024
+            dt = 1 / samples[i].frame_rate
+            sync_ms = clap_offset[i]
+            rate = samples[i].frame_rate
+            trimval = int(round(sync_ms * rate / 1000))
+            freqs, times, Sx = signal.spectrogram(np.array(raw[trimval:]), fs=dt,
+                                                  window='hanning',
+                                                  nperseg=M, noverlap=M - 100,
+                                                  detrend=False, scaling='spectrum')
+            ax[i].pcolormesh(times, freqs, 10 * np.log10(Sx), cmap='viridis')
+            ax[i].set_title("Accelerometer Spectogram")
+            ax[i].set_ylabel('Frequency [Hz]')
+            ax[i].set_xlabel('Time [s]');
+
     fig, ax = plt.subplots(nrows=len(raws), sharex=True, sharey=True)
     for i in range(len(raws)):
-        hackval = int(round(clap_offset[i] * rate / 1000))
-        librosa.display.waveplot(np.array(raws[i][hackval:]).astype('float'), sr=sample.frame_rate, ax=ax[i])
+        trimval = int(round(clap_offset[i] * rate / 1000))
+        librosa.display.waveplot(np.array(raws[i][trimval:]).astype('float'), sr=samples[i].frame_rate, ax=ax[i])
         ax[i].set(title=args.videos[i])
         ax[i].label_outer()
-    plt.show()
 
     hop_length = 1024
     chromas = []
-    fig, ax = plt.subplots(nrows=len(raws), sharey=True)
+    fig, ax = plt.subplots(nrows=len(raws), sharex=True, sharey=True)
     for i in range(len(raws)):
-        hackval = int(round(clap_offset[i] * rate / 1000))
-        chroma = librosa.feature.chroma_cqt(y=np.array(raws[i][hackval:]).astype('float'),
-                                            sr=sample.frame_rate,
+        trimval = int(round(clap_offset[i] * rate / 1000))
+        chroma = librosa.feature.chroma_cqt(y=np.array(raws[i][trimval:]).astype('float'),
+                                            sr=samples[i].frame_rate,
                                             hop_length=hop_length)
         chromas.append(chroma)
         img = librosa.display.specshow(chroma, x_axis='time',
@@ -140,6 +144,45 @@ if True:
                                        hop_length=hop_length, ax=ax[i])
         ax[i].set(title='Chroma Representation of ' + args.videos[i])
     fig.colorbar(img, ax=ax)
+
+    if False:
+        # this could possibly be used to find a series of sync offsets
+        # between audio streams, but you need the same notes ...
+        D, wp = librosa.sequence.dtw(X=chromas[0], Y=chromas[2], metric='cosine')
+        wp_s = librosa.frames_to_time(wp, sr=samples[i].frame_rate, hop_length=hop_length)
+
+        fig, ax = plt.subplots()
+        img = librosa.display.specshow(D, x_axis='time', y_axis='time', sr=samples[i].frame_rate,
+                                       cmap='gray_r', hop_length=hop_length, ax=ax)
+        ax.plot(wp_s[:, 1], wp_s[:, 0], marker='o', color='r')
+        ax.set(title='Warping Path on Acc. Cost Matrix $D$',
+               xlabel='Time $(X_2)$', ylabel='Time $(X_1)$')
+        fig.colorbar(img, ax=ax)
+
+        from matplotlib.patches import ConnectionPatch
+
+        fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(8, 4))
+
+        # Plot x_2
+        librosa.display.waveplot(np.array(raws[2][trimval:]).astype('float'), sr=samples[i].frame_rate, ax=ax2)
+        ax2.set(title='track 2')
+
+        # Plot x_1
+        librosa.display.waveplot(np.array(raws[0][trimval:]).astype('float'), sr=samples[i].frame_rate, ax=ax1)
+        ax1.set(title='track 0')
+        ax1.label_outer()
+
+        n_arrows = 20
+        for tp1, tp2 in wp_s[::len(wp_s)//n_arrows]:
+            # Create a connection patch between the aligned time points
+            # in each subplot
+            con = ConnectionPatch(xyA=(tp1, 0), xyB=(tp2, 0),
+                                  axesA=ax1, axesB=ax2,
+                                  coordsA='data', coordsB='data',
+                                  color='r', linestyle='--',
+                                  alpha=0.5)
+            ax2.add_artist(con)
+
     plt.show()
 
 # do a quick mix from starting clap
@@ -154,7 +197,7 @@ for i, v in enumerate(args.videos):
     else:
         mixed = mixed.overlay(sample[sync_ms:])
 
-if False:
+if True:
     print("playing quick sync combined audio...")
     playback.play(mixed)
 
@@ -164,8 +207,8 @@ for i, sample in enumerate(samples):
     print(args.videos[i])
     sync_ms = clap_offset[i]
     sample = sample[sync_ms:]
-    raw = sample.get_array_of_samples()
-    rate = sample.frame_rate
+    raw = raws[i]
+    rate = samples[i].frame_rate
     if ref is None:
         ref = raw
         cor = 0
