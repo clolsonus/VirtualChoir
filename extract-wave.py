cd66706f05c719b6ccc364b535036e884cef3937
Curtis L. Olson <curtolson@flightgear.org>
2020-09-17 08:56:09 -0500

    Extract the audio (as a wave file) from other movie or audio formats.

--- extract-wave.py
+++ extract-wave.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 
-# https://librosa.org/doc/latest/auto_examples/plot_music_sync.html#sphx-glr-auto-examples-plot-music-sync-py
-
-# pyrubberband may offer a higher quality stretch/compress function
-
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='virtual choir')
-parser.add_argument('videos', metavar='videos', nargs='+',
-                    help='input videos')
+parser = argparse.ArgumentParser(description='extract wave file from anything ffmpeg can grok')
+parser.add_argument('videos', metavar='videos', nargs='+', help='input videos')
 args = parser.parse_args()
 
 for v in args.videos:
