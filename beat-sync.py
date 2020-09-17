013e9cde98edff87b4037f5750da9f6e6e77a93b
Curtis L. Olson <curtolson@flightgear.org>
2020-09-17 14:40:28 -0500

    Deep dive in producing beat synced audio does something, but leaves much to be desired, and also needs some debugging.  It's possible I will shelve this idea for a while and work on the video mixing side.

--- beat-sync.py
+++ beat-sync.py
@@ -15,12 +15,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from pydub import AudioSegment, playback  # pip install pydub
+import pyrubberband as pyrb               # pip install pyrubberband
 from scipy import signal                  # spectrogram
 
 parser = argparse.ArgumentParser(description='virtual choir')
 parser.add_argument('videos', metavar='videos', nargs='+',
                     help='input videos')
 args = parser.parse_args()
+
+# function from: https://stackoverflow.com/questions/51434897/how-to-change-audio-playback-speed-using-pydub
+def change_audioseg_tempo(segment, scale):
+    y = np.array(segment.get_array_of_samples())
+    if segment.channels == 2:
+        y = y.reshape((-1, 2))
+
+    sr = segment.frame_rate
+    y_fast = pyrb.time_stretch(y, sr, scale)
+
+    channels = 2 if (y_fast.ndim == 2 and y_fast.shape[1] == 2) else 1
+    y = np.int16(y_fast * 2 ** 15)
+
+    new_seg = AudioSegment(y.tobytes(), frame_rate=sr, sample_width=2, channels=channels)
+    return new_seg
 
 # load samples, convert to mono, and normalize
 print("loading samples...")
@@ -36,11 +52,11 @@ for v in args.videos:
     raw = mono.get_array_of_samples()
     raws.append(raw)
 
-# use librosa to analyze audio streams
+# analyze audio streams (using librosa functions)
+hop_length = 512
 onset_list = []
 time_list = []
 beat_list = []
-hop_length = 512
 for i, raw in enumerate(raws):
     sr = samples[i].frame_rate
     #sync_ms = clap_offset[i]
@@ -105,17 +121,72 @@ for beats in beat_list:
         beats[i] -= offset
     print(beats)
     
-# try to find nearly matching beats between clips
-for i in range(len(beat_list)-1):
-    beats1 = beat_list[i]
-    for j in range(i+1, len(beat_list)):
-        print(i, j)
-        beats2 = beat_list[j]
-        for b1 in beats1:
-            for b2 in beats2:
+# find nearly matching beats between clips and group them
+import copy
+scratch_list = copy.deepcopy(beat_list)
+groups = []
+for i in range(len(scratch_list)-1):
+    beats1 = scratch_list[i]
+    for b1 in beats1:
+        if b1 < 0: continue
+        group = [ (i, b1) ]
+        for j in range(i+1, len(scratch_list)):
+            beats2 = scratch_list[j]
+            for n, b2 in enumerate(beats2):
                 if abs(b1 - b2) < 0.15:
                     print("  track %d vs %d: %.3f %.3f" % (i, j, b1, b2))
-        
+                    group.append( (j, b2) )
+                    beats2[n] = -1
+        if len(group) > 1:
+            groups.append(group)
+            
+print("beat groupings:")
+# compute average time for each beat group (add it and label it -1)
+for group in groups:
+    sum = 0
+    count = 0
+    for i, t in group:
+        sum += t
+        count += 1
+    average = sum / count
+    group.append( (-1, average) )
+    print(group)
+
+# make time remapping templates
+temperals = []
+map_list = []
+for i in range(len(beat_list)):
+    time_mapping = []
+    for group in groups:
+        for j, t in group[:-1]:
+            if i == j:
+                time_mapping.append( (t, group[-1][1]) )
+    time_mapping = sorted(time_mapping, key=lambda fields: fields[0])
+    map_list.append(time_mapping)
+    print("time_mapping:", time_mapping)
+
+# deep dive, try to remap the timing of the clips for alignment ... scary part!
+for i, sample in enumerate(samples):
+    new = AudioSegment.empty()
+    sr = sample.frame_rate
+    offset = (clap_offset[i] / 1000) # secs
+    trimval = int(round(clap_offset[i] * sr / 1000))
+    time_map = map_list[i]
+    for j in range(len(time_map)-1):
+        src_interval = time_map[j+1][0] - time_map[j][0]
+        dst_interval = time_map[j+1][1] - time_map[j][1]
+        scale = dst_interval / src_interval
+        print("intervals:", src_interval, dst_interval, "scale:", scale)
+        c1 = int(round( (time_map[j][0] + offset) * 1000 ))
+        c2 = int(round( (time_map[j+1][0] + offset) * 1000 ))
+        clip = sample[c1:c2]
+        print(c1, c2, len(sample), len(clip))
+        #new.extend( librosa.effects.time_stretch(np.array(clip).astype('float'), scale) )
+        new += change_audioseg_tempo(clip, scale)
+        print(len(new))
+    #playback.play(new)
+    temperals.append(new)
+     
 # plot basic clip waveforms
 fig, ax = plt.subplots(nrows=len(raws), sharex=True, sharey=True)
 for i in range(len(raws)):
@@ -127,7 +198,7 @@ for i in range(len(raws)):
     for b in beat_list[i]:
         ax[i].axvline(x=b, color='b')
 
-# work with librosa to visualize audio streams
+# visualize audio streams (using librosa functions)
 if True:
     # plot original (unaligned) onset envelope peaks
     fig, ax = plt.subplots(nrows=len(onset_list), sharex=True, sharey=True)
@@ -137,7 +208,6 @@ if True:
     # compute and plot chroma representation of clips (I notice the
     # timescale has an odd scaling, but doesn't seem to be a factor of
     # 2, or maybe it is, so ???)
-    hop_length = 512
     chromas = []
     fig, ax = plt.subplots(nrows=len(raws), sharex=True, sharey=True)
     for i in range(len(raws)):
@@ -153,6 +223,19 @@ if True:
     fig.colorbar(img, ax=ax)
 
     plt.show()
+
+# do a quick mix of temperals
+print("quick mix of temperals...")
+mixed = None
+for i, v in enumerate(args.videos):
+    print(" ", v)
+    sample = temperals[i]
+    if mixed is None:
+        mixed = sample
+    else:
+        mixed = mixed.overlay(sample)
+print("playing quick sync combined audio...")
+playback.play(mixed)
 
 # do a quick mix from starting clap
 print("quick mix based on clap sync...")
