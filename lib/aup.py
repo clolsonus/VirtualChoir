dc355bb15226f9f732053fe113897d87f146c1ae
Curtis L. Olson <curtolson@flightgear.org>
2020-10-13 16:43:51 -0500

    If an audacity .aup project file is present, honor that for timing/sync offsets and don't attempt to auto-sync.

---
+++ lib/aup.py
@@ -0,0 +1,21 @@
+import lxml.etree as ET
+import os
+
+def offsets_from_aup(audio_tracks, dir, file):
+    sync_offsets = [0] * len(audio_tracks)
+    filename = os.path.join(dir, file)
+    xml = ET.parse(filename)
+    project = xml.getroot()
+    print(project.tag)
+    for wt in project.iter('{*}wavetrack'):
+        #localname = ET.QName(wt.tag).localname
+        #print(localname)
+        basename = wt.attrib["name"]
+        wc = wt.find('{*}waveclip')
+        offset = float(wc.attrib["offset"])
+        print(basename, offset)
+        for i, name in enumerate(audio_tracks):
+            if name.startswith(basename):
+                sync_offsets[i] = -offset * 1000 # ms
+    print(sync_offsets)
+    return sync_offsets
--- sync-tracks.py
+++ sync-tracks.py
@@ -10,6 +10,7 @@ from pydub import AudioSegment, playback, scipy_effects  # pip install pydub
 from tqdm import tqdm
 
 from lib import analyze
+from lib import aup
 from lib import mixer
 from lib import video
 
@@ -20,9 +21,11 @@ args = parser.parse_args()
 # find all the project clips
 audio_extensions = [ "aac", "aiff", "m4a", "mp3", "wav" ]
 video_extensions = [ "avi", "mov", "mp4" ]
-ignore_extensions = [ "aup" ]
+audacity_extension = "aup"
+ignore_extensions = [ "lof" ]
 audio_tracks = []
 video_tracks = []
+aup_project = None
 for file in sorted(os.listdir(args.project)):
     basename, ext = os.path.splitext(file)
     if basename == "group" or basename == "final":
@@ -33,10 +36,21 @@ for file in sorted(os.listdir(args.project)):
         audio_tracks.append(file)
         if ext[1:].lower() in video_extensions:
             video_tracks.append(file)
+    elif ext[1:].lower() == audacity_extension:
+        if aup_project == None:
+            aup_project = file    
+        else:
+            print("WARNING! More than one audacity project file (.aup) found")
+            print("Using first one found:", aup_project)
     else:
         print("Unknown extenstion (skipping):", file)
 print("audio tracks:", audio_tracks)
 print("video tracks:", video_tracks)
+if aup_project:
+    print("audacity project for sync:", aup_project)
+
+# temp here
+offsets = aup.offsets_from_aup(audio_tracks, args.project, aup_project)
 
 # load audio tracks and normalize
 print("loading audio tracks...")
@@ -53,43 +67,47 @@ for track in tqdm(audio_tracks):
     
 for i, sample in enumerate(audio_samples):
     print(" ", audio_tracks[i], "rate:", sample.frame_rate, "channels:", sample.channels)
+
 print("max frame rate:", max_frame_rate)
-
-# generate mono version, set consistent sample rate, and filer for
-# analysis step
-print("Processing audio samples...")
-audio_raws = []
-for i, sample in enumerate(tqdm(audio_samples)):
+for i, sample in enumerate(audio_samples):
     audio_samples[i] = audio_samples[i].set_frame_rate(max_frame_rate)
-    mono = audio_samples[i].set_channels(1) # convert to mono
-    mono_filt = scipy_effects.band_pass_filter(mono, 130, 523) #C3-C5
-    raw = mono_filt.get_array_of_samples()
-    audio_raws.append(raw)
-
-# analyze audio streams (using librosa functions)
-audio_group = analyze.SampleGroup()
-audio_group.compute_basic(audio_samples, audio_raws)
-audio_group.compute_intensities(audio_raws)
-audio_group.compute_clarities(audio_samples, audio_raws)
-audio_group.gen_plots(audio_samples, audio_raws, audio_tracks, sync_offsets=None)
-
-# analyze video streams (using librosa functions)
-#video_group = analyze.SampleGroup()
-#video_group.compute_basic(video_samples, video_raws)
-#video_group.gen_plots(video_samples, video_raws, video_tracks, sync_offsets=None)
-
+    
 sync_offsets = []
-print("correlating audio samples")
-#audio_group.correlate_by_beats( audio_group.onset_list[0],
-#                                audio_group.time_list[0],
-#                                plot=True)
-audio_group.correlate_by_generic(audio_group.clarity_list, plot=False)
-with open(os.path.join(args.project, "audacity_import.lof"), 'w') as fp:
-    for i in range(len(audio_group.offset_list)):
-        fp.write('file "%s" offset %.3f\n' % (audio_tracks[i], audio_group.offset_list[i]))
-for i in range(len(audio_group.offset_list)):
-    sync_offsets.append( -audio_group.offset_list[i] * 1000) # ms units
+if not aup_project:
+    # let's figure out the autosync, fingers crossed!!!
+    
+    # generate mono version, set consistent sample rate, and filer for
+    # analysis step
+    print("Processing audio samples...")
+    audio_raws = []
+    for i, sample in enumerate(tqdm(audio_samples)):
+        mono = audio_samples[i].set_channels(1) # convert to mono
+        mono_filt = scipy_effects.band_pass_filter(mono, 130, 523) #C3-C5
+        raw = mono_filt.get_array_of_samples()
+        audio_raws.append(raw)
 
+    # analyze audio streams (using librosa functions)
+    audio_group = analyze.SampleGroup()
+    audio_group.compute_basic(audio_samples, audio_raws)
+    audio_group.compute_intensities(audio_raws)
+    audio_group.compute_clarities(audio_samples, audio_raws)
+    audio_group.gen_plots(audio_samples, audio_raws, audio_tracks, sync_offsets=None)
+
+    print("correlating audio samples")
+    #audio_group.correlate_by_beats( audio_group.onset_list[0],
+    #                                audio_group.time_list[0],
+    #                                plot=True)
+    audio_group.correlate_by_generic(audio_group.clarity_list, plot=False)
+    with open(os.path.join(args.project, "audacity_import.lof"), 'w') as fp:
+        for i in range(len(audio_group.offset_list)):
+            fp.write('file "%s" offset %.3f\n' % (audio_tracks[i], audio_group.offset_list[i]))
+    for i in range(len(audio_group.offset_list)):
+        sync_offsets.append( -audio_group.offset_list[i] * 1000) # ms units
+else:
+    # we found an audacity project, let's read the sync offsets from that
+    sync_offsets = aup.offsets_from_aup(audio_tracks, args.project, aup_project)
+
+        
 print("Mixing samples...")
 mixed = mixer.combine(audio_tracks, audio_samples, sync_offsets,
                       pan_range=0.25)
@@ -110,43 +128,44 @@ if len(video_tracks):
     video.render_combined_video( args.project, video_tracks, video_offsets )
     video.merge( args.project )
 
-# plot basic clip waveforms
-fig, ax = plt.subplots(nrows=len(audio_raws), sharex=True, sharey=True)
-for i in range(len(audio_raws)):
-    sr = audio_samples[i].frame_rate
-    trimval = int(round(sync_offsets[i] * sr / 1000))
-    librosa.display.waveplot(np.array(audio_raws[i][trimval:]).astype('float'), sr=audio_samples[i].frame_rate, ax=ax[i])
-    ax[i].set(title=clips[i])
-    ax[i].label_outer()
-    for b in audio_group.beat_list[i]:
-        ax[i].axvline(x=b, color='b')
-plt.show()
-
-# visualize audio streams (using librosa functions)
-audio_group.gen_plots(audio_samples, audio_raws, sync_offsets, clips)
-if True:
-    # plot original (unaligned) onset envelope peaks
-    fig, ax = plt.subplots(nrows=len(onset_list), sharex=True, sharey=True)
-    for i in range(len(onset_list)):
-        ax[i].plot(time_list[i], onset_list[i])
-
-    # compute and plot chroma representation of clips (I notice the
-    # timescale has an odd scaling, but doesn't seem to be a factor of
-    # 2, or maybe it is, so ???)
-    chromas = []
+if False:
+    # plot basic clip waveforms
     fig, ax = plt.subplots(nrows=len(audio_raws), sharex=True, sharey=True)
     for i in range(len(audio_raws)):
         sr = audio_samples[i].frame_rate
         trimval = int(round(sync_offsets[i] * sr / 1000))
-        chroma = librosa.feature.chroma_cqt(y=np.array(audio_raws[i][trimval:]).astype('float'),
-                                            sr=sr, hop_length=hop_length)
-        chromas.append(chroma)
-        img = librosa.display.specshow(chroma, x_axis='time',
-                                       y_axis='chroma',
-                                       hop_length=hop_length, ax=ax[i])
-        ax[i].set(title='Chroma Representation of ' + clips[i])
-    fig.colorbar(img, ax=ax)
-
+        librosa.display.waveplot(np.array(audio_raws[i][trimval:]).astype('float'), sr=audio_samples[i].frame_rate, ax=ax[i])
+        ax[i].set(title=clips[i])
+        ax[i].label_outer()
+        for b in audio_group.beat_list[i]:
+            ax[i].axvline(x=b, color='b')
     plt.show()
 
-    
+    # visualize audio streams (using librosa functions)
+    audio_group.gen_plots(audio_samples, audio_raws, sync_offsets, clips)
+    if True:
+        # plot original (unaligned) onset envelope peaks
+        fig, ax = plt.subplots(nrows=len(onset_list), sharex=True, sharey=True)
+        for i in range(len(onset_list)):
+            ax[i].plot(time_list[i], onset_list[i])
+
+        # compute and plot chroma representation of clips (I notice the
+        # timescale has an odd scaling, but doesn't seem to be a factor of
+        # 2, or maybe it is, so ???)
+        chromas = []
+        fig, ax = plt.subplots(nrows=len(audio_raws), sharex=True, sharey=True)
+        for i in range(len(audio_raws)):
+            sr = audio_samples[i].frame_rate
+            trimval = int(round(sync_offsets[i] * sr / 1000))
+            chroma = librosa.feature.chroma_cqt(y=np.array(audio_raws[i][trimval:]).astype('float'),
+                                                sr=sr, hop_length=hop_length)
+            chromas.append(chroma)
+            img = librosa.display.specshow(chroma, x_axis='time',
+                                           y_axis='chroma',
+                                           hop_length=hop_length, ax=ax[i])
+            ax[i].set(title='Chroma Representation of ' + clips[i])
+        fig.colorbar(img, ax=ax)
+
+        plt.show()
+
+
