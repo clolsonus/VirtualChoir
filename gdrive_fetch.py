2e53c15cfedfb5b34c4e99b695b479efd694f619
Curtis L. Olson <curtolson@flightgear.org>
2020-10-27 18:32:10 -0500

    cleanups.

--- gdrive_fetch.py
+++ gdrive_fetch.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
 import argparse
-import json
-import lxml.etree as ET
-import os
 
 from services import common
 from services import gdrive
@@ -12,4 +9,5 @@ parser = argparse.ArgumentParser(description='google folder public url')
 parser.add_argument('url', help='google drive folder url')
 args = parser.parse_args()
 
-gdrive.sync_folder(args.url)
+gd = gdrive.gdrive()
+gd.sync_folder(args.url)
