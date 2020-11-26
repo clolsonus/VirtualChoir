de39e6f5a881f09023af3663e4d0b402518a9055
Curtis L. Olson <curtolson@flightgear.org>
2020-11-26 13:13:25 -0600

    Implment a "neater" grid layout system.

--- lib/video_grid.py
+++ lib/video_grid.py
@@ -1,10 +1,11 @@
+import math
 import random
 
 from .logger import log
 
 class VideoGrid:
     def __init__(self, videos, output_w, output_h, border):
-        # plan the grid
+        log("Setting up the video grid:")
         self.output_w = output_w
         self.output_h = output_h
         self.border = border
@@ -30,27 +31,52 @@ class VideoGrid:
             log("landscape dominant input videos")
 
         num_good_videos = sum(v.reader is not None for v in videos)
-        self.cols = 1
-        self.rows = 1
-        while self.cols * self.rows < num_good_videos:
-            if self.cell_landscape:
-                if self.cols <= self.rows:
-                    self.cols += 1
-                else:
-                    self.rows += 1
-            else:
-                if self.cols < self.rows*4:
-                    self.cols += 1
-                else:
-                    self.rows += 1
-        log("video grid (self.rows x self.cols):", self.rows, "x", self.cols)
-        self.grid_w = int(output_w / self.cols)
-        self.grid_h = int(output_h / self.rows)
-        self.cell_w = (output_w - self.border*(self.cols+1)) / self.cols
-        self.cell_h = (output_h - self.border*(self.rows+1)) / self.rows
-        cell_aspect = self.cell_w / self.cell_h
-        print("  grid size:", self.grid_w, "x", self.grid_h)
-        print("  cell size:", self.cell_w, "x", self.cell_h, "aspect:", cell_aspect)
+        log("Number of videos to place:", num_good_videos)
+        if self.cell_landscape:
+            self.num_rows = int(math.sqrt(num_good_videos))
+            cols = self.num_rows
+            remainder = num_good_videos - self.num_rows*self.num_rows
+        else:
+            self.num_rows = int(math.sqrt(num_good_videos / 2))
+            cols = self.num_rows * 2
+            remainder = num_good_videos - 2*self.num_rows*self.num_rows
+        # setup basic row records
+        self.rows = []
+        for i in range(self.num_rows):
+            row = { "cols": cols }
+            self.rows.append(row)
+        # try for an appoximation to a "nice" arrangement
+        while remainder > 0:
+            for i in range(self.num_rows):
+                # extend odd rows first
+                if i % 2 != 0 and remainder > 0:
+                    self.rows[i]["cols"] += 1
+                    remainder -= 1
+            for i in range(self.num_rows):
+                # extend even rows next
+                if i % 2 == 0 and remainder > 0:
+                    self.rows[i]["cols"] += 1
+                    remainder -= 1
+        if remainder > 0:
+            log("OOPS, NON ZERO REMAINDER, SOMETHING BAD HAPPENED PLANNING THE GRID")
+            quit()
+        for i in range(self.num_rows):
+            row = self.rows[i]
+            cols = row["cols"]
+            row["grid_w"] = int(output_w / cols)
+            row["grid_h"] = int(output_h / self.num_rows)
+            row["cell_w"] = (self.output_w - self.border*(cols+1)) / cols
+            row["cell_h"] = (self.output_h - self.border*(self.num_rows+1)) / self.num_rows
+            cell_aspect = row["cell_w"] / row["cell_h"]
+            print("row:", i, "grid size:", row["grid_w"], "x", row["grid_h"])
+            # print("  cell size:", self.cell_w, "x", self.cell_h, "aspect:", cell_aspect)
+
+        log("video grid layout:")
+        for row in self.rows:
+            s = ""
+            for col in range(row["cols"]):
+                s += "x "
+            log(" ", s)
 
     def update(self, videos, output_time):
         # compute placement/size for each frame (static grid strategy)
@@ -61,19 +87,23 @@ class VideoGrid:
         if self.even_offset < self.output_w:
             self.even_offset = self.output_w
         max_step = self.output_w * 0.010
-        if self.cols >= 2 and self.rows >= 2:
-            random_start = True
+        if self.num_rows >= 2:
+            animated_start = True
         else:
-            random_start = False
+            animated_start = False
         sorted_vids = sorted(videos, key=lambda x: x.sort_order)
         for v in sorted_vids:
+            row_info = self.rows[row]
+            cols = row_info["cols"]
+            cell_w = row_info["cell_w"]
+            cell_h = row_info["cell_h"]
             if v.reader is None:
                 continue
             if v.raw_frame is not None:
                 frame = v.raw_frame
                 # target grid location
-                x = self.border + col * (self.cell_w + self.border)
-                y = self.border + row * (self.cell_h + self.border)
+                x = self.border + col * (cell_w + self.border)
+                y = self.border + row * (cell_h + self.border)
                 # if frame.shape[1] < self.cell_w:
                 #     gap = (self.cell_w - frame.shape[1]) * 0.5
                 #     x += gap
@@ -89,9 +119,9 @@ class VideoGrid:
                     if v.place_x is None:
                         if True or row % 2 == 0:
                             v.place_x = self.even_offset
-                            self.even_offset += (self.cell_w + self.border)
+                            self.even_offset += (cell_w + self.border)
                         else:
-                            self.odd_offset -= self.cell_w
+                            self.odd_offset -= cell_w
                             v.place_x = self.odd_offset
                             self.odd_offset -= self.border
                     if v.place_y is None:
@@ -105,10 +135,10 @@ class VideoGrid:
                     if dy < -max_step: dy = -max_step
                     v.place_x += dx
                     v.place_y += dy
-                v.size_w = self.cell_w
-                v.size_h = self.cell_h
+                v.size_w = cell_w
+                v.size_h = cell_h
                 col += 1
-                if col >= self.cols:
+                if col >= cols:
                     col = 0
                     row += 1
 
