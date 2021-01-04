9e666a45d1f74c43ee64b55ffa8ec2c3e9379f2a
Curtis L. Olson <curtolson@flightgear.org>
2021-01-03 19:20:33 -0600

    Add rows and fit options.

--- sync-tracks.py
+++ sync-tracks.py
@@ -31,6 +31,8 @@ parser.add_argument('--resolution', default='1080p',
                     choices=['480p', '720p', '1080p', '1440p'],
                     help='video output resolution')
 parser.add_argument('--rows', type=int, help='request specific number of video rows')
+parser.add_argument('--fit', default='face', choices=['fit', 'zoom', 'face'],
+                    help='video scaling/cropping strategy')
 args = parser.parse_args()
 
 log("Begin processing job", fancy=True)
@@ -187,8 +189,7 @@ if len(all_video_tracks) and not args.no_video:
     # render the new combined video
     video.render_combined_video( args.project, args.resolution, results_dir,
                                  all_video_tracks, video_offsets,
-                                 hints=hint_dict,
-                                 rows=args.rows,
+                                 hints=hint_dict, rows=args.rows, fit=args.fit,
                                  title_page=title_page,
                                  credits_page=credits_page )
     video.merge( args.project, results_dir )
